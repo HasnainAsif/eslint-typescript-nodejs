a3b036665e9fc6fe5d8e61600316f470e24899f9
Hasnain Asif <hasnain.asif@invozone.com>
2022-01-25 15:03:38 +0500

    eslinting with typescript and nodejs

---
+++ index.ts
@@ -0,0 +1,6 @@
+// const restify = require("url");
+const PORT: number = 5000;
+
+// const app = restify.createServer();
+// app.get("/", (req, res) => res.send({ hello: "World" }));
+// app.listen(PORT);
